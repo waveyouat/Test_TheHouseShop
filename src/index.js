11f5b9adfd90e4c6c313b940c096fce3415addd4
waveyouat <waveyouat@gmail.com>
2019-05-29 21:07:52 +0200

    App component refactoring
    Refactoring of the App component, from functional component created by create-react-app to a component extending React Component Class to better manage state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-      <h2>Who's coming?</h2>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+        <h2>Who's coming?</h2>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
